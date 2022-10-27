e7417edb0342a48a8e19023f6dabd9ea3ffe1a37
srikanthgangula <108477399+srikanthgangula@users.noreply.github.com>
2022-10-27 11:24:31 +0530

    Update activity.sh

--- activity.sh
+++ activity.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 az group create --name Helloworld --location AustraliaEast
-
-##vm creation
-az vm create --resource-group Helloworld --name HelloVM --image Debian --admin-username srikanth --generate-ssh-keys
